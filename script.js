efa3bebe5b3fec2b90b98d8ee71a9325567e49b5
Nikita Gavrilov <nikita.gavrilov@narg.ee>
2025-03-28 14:35:52 +0200

    Added header, hero and applied basic css to them + hero photo

---
+++ script.js
@@ -0,0 +1,5 @@
+const toMansionsButton = document.getElementById("redirectToMansionsPage")
+
+toMansionsButton.addEventListener("click", (event) => {
+    window.location.href = "./pages/mansions.html";
+})
